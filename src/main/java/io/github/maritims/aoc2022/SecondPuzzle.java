0ca026b629fa661123d1c15f682a064d4f38a13d
Martin Severin Steffensen <maritim@gmail.com>
2022-12-03 23:58:17 +0100

    Refactored day 2 part 1 to use bitwise AND with Mod 3.

--- src/main/java/io/github/maritims/aoc2022/SecondPuzzle.java
+++ src/main/java/io/github/maritims/aoc2022/SecondPuzzle.java
@@ -9,15 +9,23 @@ public class SecondPuzzle extends Puzzle {
         int points = 0;
         for(String round : getFileContent()) {
             String[] moves = round.split(" ");
-            int enemy = moves[0].charAt(0);
-            int you = moves[1].charAt(0);
-            int outcome = Math.abs(enemy - you);
-            if(outcome == 23) {
-                points += 3;
-            } else if(outcome == 21 || outcome == 24) {
-                points += 6;
-            }
-            points += you - 87;
+            int originalEnemyMove = moves[0].charAt(0);
+            int originalPlayerMove = moves[1].charAt(0);
+
+            // (A, B, C) & 3 -> (65, 66, 67) & 3 -> (1, 2, 3) -> Subtract 1 to use wrapping -> (0, 1, 2).
+            int simplifiedEnemyMove = originalEnemyMove & 3;
+
+            // (X, Y, Z) & 3 -> (88, 89, 90) & 3 -> (0, 1, 2).
+            int simplifiedPlayerMove = originalPlayerMove & 3;
+
+            // 1 (Enemy rock)       Mod 3 = 1 (Player paper)
+            // 2 (Enemy paper)      Mod 3 = 2 (Player scissors)
+            // 3 (Enemy scissors )  Mod 3 = 0 (Player rock)
+            // Utilise zero multiplication for simplicity when calculating points.
+            int pointsForVictory = (simplifiedEnemyMove % 3 == simplifiedPlayerMove ? 1 : 0) * 6;
+            int pointsForDraw = (simplifiedEnemyMove - 1 == simplifiedPlayerMove ? 1 : 0) * 3;
+            int pointsForLoss = (originalPlayerMove & 3) + 1;
+            points += pointsForVictory + pointsForDraw + pointsForLoss;
         }
         return points;
     }
