1df7941b5d2ff9a41e653d81c20d05045f74cb48
Martin Severin Steffensen <maritim@gmail.com>
2022-12-03 19:41:41 +0100

    Day 3 part 1 now uses HashSet instead of two loops.

--- src/main/java/io/github/maritims/aoc2022/ThirdPuzzle.java
+++ src/main/java/io/github/maritims/aoc2022/ThirdPuzzle.java
@@ -16,20 +16,18 @@ public class ThirdPuzzle extends Puzzle {
     public Integer solvePartOne() {
         int sum = 0;
         for (String rucksack : getFileContent()) {
-            String[] compartments = new String[]{rucksack.substring(0, rucksack.length() / 2), rucksack.substring(rucksack.length() / 2)};
-            List<Character> sharedItems = new ArrayList<>();
-            for(char item1 : compartments[0].toCharArray()) {
-                if(sharedItems.contains(item1)) {
-                    continue;
-                }
-                for(char item2 : compartments[1].toCharArray()) {
-                    if(item1 == item2) {
-                        sharedItems.add(item1);
-                        break;
-                    }
-                }
-            }
-            sum += sharedItems.stream().mapToInt(this::getPriority).sum();
+            List<String> compartments = Arrays.asList(rucksack.substring(0, rucksack.length() / 2), rucksack.substring(rucksack.length() / 2));
+            HashSet<Integer> priorities = compartments.get(0)
+                    .chars()
+                    .mapToObj(c -> (char) c)
+                    .map(this::getPriority)
+                    .collect(Collectors.toCollection(HashSet::new));
+            priorities.retainAll(compartments.get(1)
+                    .chars()
+                    .mapToObj(c -> (char) c)
+                    .map(this::getPriority)
+                    .collect(Collectors.toCollection(HashSet::new)));
+            sum += priorities.stream().mapToInt(i -> i).sum();
         }
         return sum;
     }
@@ -49,7 +47,7 @@ public class ThirdPuzzle extends Puzzle {
     protected Character getCommonItemInGroup(List<List<Character>> itemsInGroup) {
         Iterator<List<Character>> iterator = itemsInGroup.iterator();
         List<Character> commons = iterator.next();
-        while(iterator.hasNext()) {
+        while (iterator.hasNext()) {
             commons.retainAll(iterator.next());
         }
         return commons.stream().distinct().collect(Collectors.toList()).get(0);
@@ -59,7 +57,7 @@ public class ThirdPuzzle extends Puzzle {
     public Integer solvePartTwo() {
         List<String> fileContent = getFileContent();
         List<List<String>> groups = new ArrayList<>();
-        for(int i = 0; i < fileContent.size(); i += 3) {
+        for (int i = 0; i < fileContent.size(); i += 3) {
             groups.add(fileContent.subList(i, i + 3));
         }
 
