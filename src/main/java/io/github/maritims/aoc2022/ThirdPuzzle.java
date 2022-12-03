27fe8b60f3a9b705b5b9f3ab04b9e573cd5333fe
Martin Severin Steffensen <maritim@gmail.com>
2022-12-03 15:30:00 +0100

    Solved day 3 part 2.

--- src/main/java/io/github/maritims/aoc2022/ThirdPuzzle.java
+++ src/main/java/io/github/maritims/aoc2022/ThirdPuzzle.java
@@ -1,11 +1,15 @@
 package io.github.maritims.aoc2022;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class ThirdPuzzle extends Puzzle {
     public ThirdPuzzle(String fileName) {
         super(fileName);
+    }
+
+    protected int getPriority(Character c) {
+        return c - (Character.isUpperCase(c) ? 'A' - 27 : 'a' - 1);
     }
 
     @Override
@@ -25,13 +29,44 @@ public class ThirdPuzzle extends Puzzle {
                     }
                 }
             }
-            sum += sharedItems.stream().mapToInt(c -> c - (Character.isUpperCase(c) ? 'A' - 27 : 'a' - 1)).sum();
+            sum += sharedItems.stream().mapToInt(this::getPriority).sum();
         }
         return sum;
     }
 
+    protected List<Character> getItemsInRucksack(String rucksack) {
+        return rucksack.chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.toList());
+    }
+
+    protected List<List<Character>> getItemsInGroup(List<String> group) {
+        return group.stream()
+                .map(this::getItemsInRucksack)
+                .collect(Collectors.toList());
+    }
+
+    protected Character getCommonItemInGroup(List<List<Character>> itemsInGroup) {
+        Iterator<List<Character>> iterator = itemsInGroup.iterator();
+        List<Character> commons = iterator.next();
+        while(iterator.hasNext()) {
+            commons.retainAll(iterator.next());
+        }
+        return commons.stream().distinct().collect(Collectors.toList()).get(0);
+    }
+
     @Override
     public Integer solvePartTwo() {
-        return null;
+        List<String> fileContent = getFileContent();
+        List<List<String>> groups = new ArrayList<>();
+        for(int i = 0; i < fileContent.size(); i += 3) {
+            groups.add(fileContent.subList(i, i + 3));
+        }
+
+        return groups.stream()
+                .map(this::getItemsInGroup)
+                .map(this::getCommonItemInGroup)
+                .mapToInt(this::getPriority)
+                .sum();
     }
 }
--- src/test/java/io/github/maritims/aoc2022/PuzzleTest.java
+++ src/test/java/io/github/maritims/aoc2022/PuzzleTest.java
@@ -21,9 +21,26 @@ public class PuzzleTest {
         );
     }
 
+    public static Stream<Arguments> solvePartTwo() {
+        return Stream.of(
+                Arguments.arguments(1, FirstPuzzle.class, "first/example.txt", 45000),
+                Arguments.arguments(1, FirstPuzzle.class, "first/input.txt", 200116),
+                Arguments.arguments(2, SecondPuzzle.class, "second/example.txt", 12),
+                Arguments.arguments(2, SecondPuzzle.class, "second/input.txt", 12424),
+                Arguments.arguments(3, ThirdPuzzle.class, "third/example.txt", 70),
+                Arguments.arguments(3, ThirdPuzzle.class, "third/input.txt", 2604)
+        );
+    }
+
     @ParameterizedTest(name = "Day {0}, part 1, {2}")
     @MethodSource
     void solvePartOne(int day, Class<Puzzle> puzzleClass, String filePath, int expectedResult) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
         assertEquals(expectedResult, puzzleClass.getConstructor(String.class).newInstance(filePath).solvePartOne());
+    }
+
+    @ParameterizedTest(name = "Day {0}, part 2, {2}")
+    @MethodSource
+    void solvePartTwo(int day, Class<Puzzle> puzzleClass, String filePath, int expectedResult) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        assertEquals(expectedResult, puzzleClass.getConstructor(String.class).newInstance(filePath).solvePartTwo());
     }
 }
