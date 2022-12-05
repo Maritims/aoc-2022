ee7899e4adb6eada8dae2ebca8a66f332ca17701
Martin Severin Steffensen <maritim@gmail.com>
2022-12-05 22:28:24 +0100

    Refactored day 5.

--- src/main/java/io/github/maritims/aoc2022/FifthPuzzle.java
+++ src/main/java/io/github/maritims/aoc2022/FifthPuzzle.java
@@ -9,6 +9,48 @@ public class FifthPuzzle extends Puzzle<String> {
 
     public static final Pattern CRATE_NUMBER_PATTERN = Pattern.compile("\\s+(\\d+)\\s");
 
+    interface CrateMover {
+        int[] getInstructions(String instructions);
+        void moveCrates(List<Character> sourceStack, List<Character> destinationStack, int quantity);
+        String inspectTop(Map<Integer, List<Character>> stacks);
+    }
+
+    static class CrateMover9000 implements CrateMover {
+        @Override
+        public int[] getInstructions(String instructions) {
+            String[] parts = instructions.split(" ");
+            return new int[] { Integer.parseInt(parts[1]), Integer.parseInt(parts[3]) - 1, Integer.parseInt(parts[5]) - 1 };
+        }
+
+        @Override
+        public void moveCrates(List<Character> sourceStack, List<Character> destinationStack, int quantity) {
+            Iterator<Character> sourceIterator = sourceStack.iterator();
+            for(int i = 0; i < quantity; i++) {
+                Character crate = sourceIterator.next();
+                sourceIterator.remove();
+                destinationStack.add(0, crate);
+            }
+        }
+
+        @Override
+        public String inspectTop(Map<Integer, List<Character>> stacks) {
+            return stacks.values()
+                    .stream()
+                    .map(stack -> stack.size() == 0 ? "" : stack.get(0))
+                    .map(Object::toString)
+                    .collect(Collectors.joining(""));
+        }
+    }
+
+    static class CrateMover9001 extends CrateMover9000 implements CrateMover {
+        @Override
+        public void moveCrates(List<Character> sourceStack, List<Character> destinationStack, int quantity) {
+            List<Character> crates = sourceStack.subList(0, quantity);
+            destinationStack.addAll(0, crates);
+            sourceStack.subList(0, quantity).clear();
+        }
+    }
+
     public Map<Integer, List<Character>> getCrateStacks(Iterator<String> iterator) {
         Map<Integer, List<Character>> crateStacks = new HashMap<>();
 
@@ -37,63 +79,25 @@ public class FifthPuzzle extends Puzzle<String> {
         return crateStacks;
     }
 
+    public String moveCrates(String filePath, CrateMover crane) {
+        Iterator<String> iterator = getFileContent(filePath).iterator();
+        Map<Integer, List<Character>> stacks = getCrateStacks(iterator);
+        List<String> remainingLines = new ArrayList<>();
+        iterator.forEachRemaining(remainingLines::add);
+
+        remainingLines.stream()
+                .map(crane::getInstructions)
+                .forEach(instructions -> crane.moveCrates(stacks.get(instructions[1]), stacks.get(instructions[2]), instructions[0]));
+        return crane.inspectTop(stacks);
+    }
+
     @Override
     public String solvePartOne(String filePath) {
-        Iterator<String> iterator = getFileContent(filePath).iterator();
-        Map<Integer, List<Character>> crateStacks = getCrateStacks(iterator);
-
-        while(iterator.hasNext()) {
-            String line = iterator.next();
-            String[] instructions = line.split(" ");
-            int crates = Integer.parseInt(instructions[1]);
-            int source = Integer.parseInt(instructions[3]) - 1;
-            int destination = Integer.parseInt(instructions[5]) - 1;
-
-            Iterator<Character> sourceIterator = crateStacks.get(source).iterator();
-            int i = 0;
-            Character crate;
-            while(i < crates) {
-                // Pick up the crate
-                crate = sourceIterator.next();
-                sourceIterator.remove();
-
-                // Put the crate down
-                crateStacks.get(destination).add(0, crate);
-
-                // Go to the next crate
-                i++;
-            }
-            iterator.remove();
-        }
-        return crateStacks.values()
-                .stream()
-                .map(stack -> stack.size() == 0 ? "" : stack.get(0))
-                .map(Object::toString)
-                .collect(Collectors.joining(""));
+        return moveCrates(filePath, new CrateMover9000());
     }
 
     @Override
     public String solvePartTwo(String filePath) {
-        Iterator<String> iterator = getFileContent(filePath).iterator();
-        Map<Integer, List<Character>> crateStacks = getCrateStacks(iterator);
-
-        while(iterator.hasNext()) {
-            String line = iterator.next();
-            String[] instructions = line.split(" ");
-            int quantity = Integer.parseInt(instructions[1]);
-            int source = Integer.parseInt(instructions[3]) - 1;
-            int destination = Integer.parseInt(instructions[5]) - 1;
-
-            List<Character> crates = crateStacks.get(source).subList(0, quantity);
-            crateStacks.get(destination).addAll(0, crates);
-            crateStacks.get(source).subList(0, quantity).clear();
-
-            iterator.remove();
-        }
-        return crateStacks.values()
-                .stream()
-                .map(stack -> stack.size() == 0 ? "" : stack.get(0))
-                .map(Object::toString)
-                .collect(Collectors.joining(""));
+        return moveCrates(filePath, new CrateMover9001());
     }
 }
