90850c39d4e72590e5f7d580554e25ba6272f8e7
Martin Severin Steffensen <maritim@gmail.com>
2022-12-05 23:52:06 +0100

    Refactored day 2.

--- src/main/java/io/github/maritims/aoc2022/SecondPuzzle.java
+++ src/main/java/io/github/maritims/aoc2022/SecondPuzzle.java
@@ -1,89 +1,66 @@
 package io.github.maritims.aoc2022;
 
 public class SecondPuzzle extends Puzzle<Integer> {
-    /**
-     * <p>This method takes an {@code int} representing the original move, and returns the simplified version.</p>
-     * <br>
-     * <p>The simplified version is calculated by performing a bitwise AND operation with the value 3.</p>
-     * @param originalMove ASCII code for a character representing the original move.
-     * @return ∈ {1, 2, 3}
-     */
-    public int getSimplifiedMove(int originalMove) {
-        return originalMove & 3;
-    }
 
-    @Override
-    public Integer solvePartOne(String filePath) {
-        int points = 0;
-        for(String round : getFileContent(filePath)) {
-            String[] moves = round.split(" ");
-            int originalPlayerMove = moves[1].charAt(0);
-            int simplifiedEnemyMove = getSimplifiedMove(moves[0].charAt(0));
-            int simplifiedPlayerMove = getSimplifiedMove(originalPlayerMove) + 1; // Add 1 to simplify ternary operations -> (1, 2, 3).
+    static class Rochambeau {
+        /**
+         * Converts the original ASCII code by performing a bitwise AND operation with the value 3.
+         * @param asciiCode ASCII code for a character representing the move. Expects either A, B, C, X, Y or Z.
+         * @return ∈ {1, 2, 3}
+         */
+        private static Integer getNormalisedCode(int asciiCode) {
+            return asciiCode & 3;
+        }
 
+        /**
+         * Plays a game of Rochambeau and returns the points from the player's perspective.
+         * @param enemyMove ∈ {1 (Rock), 2 (Paper), 3 (Scissors)}
+         * @param playerMove ∈ {1 (Paper), 2 (Scissors) or 0 (Rock)}
+         * @return The result of the game in points. 6 (victory), 3 (draw), 0 (loss) plus 1 (rock), 2 (paper) or 3 (scissors).
+         */
+        public static Integer play(int enemyMove, int playerMove) {
+            playerMove = getNormalisedCode(playerMove) + 1;
+            enemyMove = getNormalisedCode(enemyMove);
             // 1 (Enemy rock)       Mod 3 = 1 (Player paper)
             // 2 (Enemy paper)      Mod 3 = 2 (Player scissors)
             // 3 (Enemy scissors)   Mod 3 = 0 (Player rock)
             // Utilise zero multiplication for simplicity when calculating points.
-            int pointsForVictory = (simplifiedEnemyMove % 3 == (simplifiedPlayerMove - 1) ? 1 : 0) * 6;
-            int pointsForDraw = (simplifiedEnemyMove == simplifiedPlayerMove ? 1 : 0) * 3;
-            int pointsForLoss = (originalPlayerMove & 3) + 1;
-            points += pointsForVictory + pointsForDraw + pointsForLoss;
+            int pointsForVictory = (enemyMove % 3 == (playerMove - 1) ? 1 : 0) * 6;
+            int pointsForDraw = (enemyMove == playerMove ? 1 : 0) * 3;
+            int pointsForLoss = playerMove;
+            return pointsForVictory + pointsForDraw + pointsForLoss;
         }
-        return points;
+
+        public static Integer playForOutcome(int enemyMove, int outcome) {
+            return play(enemyMove, getMoveForOutcome(enemyMove, outcome));
+        }
+
+        /**
+         * Gets the required player move for the desired outcome.
+         * @param enemyMove ASCII code representing the enemy move ∈ {A, B, C, X, Y, Z}
+         * @param outcome ASCII code representing the desired outcome ∈ {A, B, C, X, Y, Z}
+         * @return Required player move ∈ {0, 1, 2}
+         */
+        public static Integer getMoveForOutcome(int enemyMove, int outcome) {
+            enemyMove = getNormalisedCode(enemyMove);
+            outcome = getNormalisedCode(outcome) + 1;
+            return (outcome + enemyMove) % 3;
+        }
     }
 
-    /**
-     * <p>This method takes an {@code int} representing the original outcome, performs a bitwise AND operation, adds 1 and returns the outcome.</p>
-     * <br>
-     * <p>Output of AND operation ∈ {0, 1, 2}.</p>
-     * <br>
-     * <p>The 1 is added to simplify Mod operations with the result of {@link #getSimplifiedMove(int)}.</p>
-     * @param originalOutcome ASCII code for a character representing the outcome.
-     * @return ∈ {1, 2, 3}
-     */
-    public int getSimplifiedOutcome(int originalOutcome) {
-        return (originalOutcome & 3) + 1;
-    }
-
-    /**
-     * <p>This method takes two {@code int} representing a desired outcome and an enemy move, and returns the required player move to achieve this outcome.</p>
-     * <br>
-     * <p>The required player move is calculated by adding the outcome, the enemy move and performing Mod 3 on the result.</p>
-     * @param simplifiedOutcome Desired outcome where the value ∈ {1, 2, 3}
-     * @param simplifiedEnemyMove Enemy move where the value ∈ {1, 2, 3}
-     * @return ∈ {0, 1, 2}
-     */
-    public int getRequiredPlayerMoveForOutcome(int simplifiedOutcome, int simplifiedEnemyMove) {
-        return (simplifiedOutcome + simplifiedEnemyMove) % 3;
-    }
-
-    /**
-     * <p>This method takes two {@code int} representing a simplified move and a simplified outcome, calculates the points for the move and outcome and returns the sum.</p>
-     * <br>
-     * <p>The points for the move are calculated by adding 1 to the move as the possible points for player moves ∈ {1, 2, 3}.</p>
-     * <br>
-     * <p>The points for the outcome are calculated by subtracting 1 from the outcome and multiplying by 3 as points for outcomes are either 0 (loss), 1 (draw) or 3 (win).</p>
-     * @param simplifiedMove Player move where the value ∈ {0, 1, 2}
-     * @param simplifiedOutcome Desired outcome where the value ∈ {1, 2, 3}
-     * @return The sum of the points for the move and the points for the outcome.
-     */
-    public int getPoints(int simplifiedMove, int simplifiedOutcome) {
-        int pointsForMove = simplifiedMove + 1;
-        int pointsForOutcome = (simplifiedOutcome - 1) * 3;
-        return pointsForMove + pointsForOutcome;
+    @Override
+    public Integer solvePartOne(String filePath) {
+        return getFileContent(filePath).stream()
+                .map(round -> round.split(" "))
+                .mapToInt(moves -> Rochambeau.play(moves[0].charAt(0), moves[1].charAt(0)))
+                .sum();
     }
 
     @Override
     public Integer solvePartTwo(String filePath) {
-        int points = 0;
-        for(String round : getFileContent(filePath)) {
-            String[] moves = round.split(" ");
-            int simplifiedEnemyMove = getSimplifiedMove(moves[0].charAt(0));
-            int simplifiedOutcome = getSimplifiedOutcome(moves[1].charAt(0));
-            int simplifiedPlayerMoveForOutcome = getRequiredPlayerMoveForOutcome(simplifiedOutcome, simplifiedEnemyMove);
-            points += getPoints(simplifiedPlayerMoveForOutcome, simplifiedOutcome);
-        }
-        return points;
+        return getFileContent(filePath).stream()
+                .map(round -> round.split(" "))
+                .mapToInt(moves -> Rochambeau.playForOutcome(moves[0].charAt(0), moves[1].charAt(0)))
+                .sum();
     }
 }
