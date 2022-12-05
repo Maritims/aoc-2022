8098bbc335d42f7892701905fd3cac2cf9ef59c1
Martin Severin Steffensen <maritim@gmail.com>
2022-12-06 00:02:17 +0100

    Comment fix.

--- src/main/java/io/github/maritims/aoc2022/SecondPuzzle.java
+++ src/main/java/io/github/maritims/aoc2022/SecondPuzzle.java
@@ -5,8 +5,8 @@ public class SecondPuzzle extends Puzzle<Integer> {
     static class Rochambeau {
         /**
          * Converts the original ASCII code by performing a bitwise AND operation with the value 3.
-         * @param asciiCode ASCII code for a character representing the move. Expects either A, B, C, X, Y or Z.
-         * @return ∈ {1, 2, 3}
+         * @param asciiCode ASCII code for a character representing moves and outcomes ∈ {A, B, C, X, Y, Z}
+         * @return {A, B, C} = {1, 2, 3} and {X, Y, Z} = {0, 1, 2}
          */
         private static Integer getNormalisedCode(int asciiCode) {
             return asciiCode & 3;
@@ -14,8 +14,8 @@ public class SecondPuzzle extends Puzzle<Integer> {
 
         /**
          * Plays a game of Rochambeau and returns the points from the player's perspective.
-         * @param enemyMove ∈ {1 (Rock), 2 (Paper), 3 (Scissors)}
-         * @param playerMove ∈ {1 (Paper), 2 (Scissors), 0 (Rock)}
+         * @param enemyMove ∈ {A = 65 = Rock, B = 66 = Paper, C = 67 = Scissors}
+         * @param playerMove ∈ {X = 88 = Paper, Y = 89 = Scissors, Z = 90 = Rock)}
          * @return The result of the game in points. 6 (victory), 3 (draw), 0 (loss) plus 1 (rock), 2 (paper) or 3 (scissors).
          */
         public static Integer play(int enemyMove, int playerMove) {
@@ -31,14 +31,20 @@ public class SecondPuzzle extends Puzzle<Integer> {
             return pointsForVictory + pointsForDraw + pointsForLoss;
         }
 
+        /**
+         * Plays a game of Rochambeau and returns the points from the player's perspective, but forces the desired outcome.
+         * @param enemyMove ∈ {A = 65 = Rock, B = 66 = Paper, C = 67 = Scissors}
+         * @param outcome ∈ {X = 88 = Loss, Y = 89 = Draw, Z = 90 = Victory)}
+         * @return
+         */
         public static Integer playForOutcome(int enemyMove, int outcome) {
             return play(enemyMove, getMoveForOutcome(enemyMove, outcome));
         }
 
         /**
          * Gets the required player move for the desired outcome.
-         * @param enemyMove ASCII code representing the enemy move ∈ {A, B, C, X, Y, Z}
-         * @param outcome ASCII code representing the desired outcome ∈ {A, B, C, X, Y, Z}
+         * @param enemyMove ASCII code representing the enemy move ∈ {A = 65, B = 66, C = 67}
+         * @param outcome ASCII code representing the desired outcome ∈ {X = 88, Y = 89, Z = 90}
          * @return Required player move ∈ {0, 1, 2}
          */
         public static Integer getMoveForOutcome(int enemyMove, int outcome) {
