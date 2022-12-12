b6b4023367abb32e9341cf0c4bbf8f77f5ddc30a
Martin Severin Steffensen <maritim@gmail.com>
2022-12-12 23:34:47 +0100

    CPU is now run by the clock circuit.

--- src/main/java/io/github/maritims/aoc2022/TenthPuzzle.java
+++ src/main/java/io/github/maritims/aoc2022/TenthPuzzle.java
@@ -4,10 +4,36 @@ import java.util.stream.IntStream;
 
 public class TenthPuzzle extends Puzzle<Integer> {
     static class CPU {
-        private int cycles = 0;
         private int X = 1;
+
+        public int getX() {
+            return X;
+        }
+
+        public void addX(int V) {
+            X += V;
+        }
+    }
+
+    static class CRT {
+        private boolean[][] pixels = new boolean[6][40];
+
+        public void draw(int x, int y, boolean isLit) {
+            pixels[y][x] = isLit;
+        }
+    }
+
+    static class ClockCircuit {
+        private int cycles = 0;
         private int nextInterestingCycle = 20;
         private int totalSignalStrength = 0;
+        private final CPU cpu;
+        private final CRT crt;
+
+        public ClockCircuit(CPU cpu, CRT crt) {
+            this.cpu = cpu;
+            this.crt = crt;
+        }
 
         public int getTotalSignalStrength() {
             return totalSignalStrength;
@@ -16,7 +42,7 @@ public class TenthPuzzle extends Puzzle<Integer> {
         private void cycle() {
             cycles++;
             if (cycles == nextInterestingCycle) {
-                totalSignalStrength += cycles * X;
+                totalSignalStrength += cycles * cpu.getX();
                 nextInterestingCycle += 40;
             }
         }
@@ -26,16 +52,16 @@ public class TenthPuzzle extends Puzzle<Integer> {
                 cycle();
             } else if(instruction.startsWith("addx")) {
                 IntStream.of(0, 2).forEach(i -> cycle());
-                X += Integer.parseInt(instruction.split(" ")[1]);
+                cpu.addX(Integer.parseInt(instruction.split(" ")[1]));
             }
         }
     }
 
     @Override
     public Integer solvePartOne(String filePath) {
-        CPU cpu = new CPU();
-        getFileContent(filePath).forEach(cpu::run);
-        return cpu.getTotalSignalStrength();
+        ClockCircuit clockCircuit = new ClockCircuit(new CPU(), new CRT());
+        getFileContent(filePath).forEach(clockCircuit::run);
+        return clockCircuit.getTotalSignalStrength();
     }
 
     @Override
