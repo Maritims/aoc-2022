743d125e51f98e99eacfaae97f496794e7a46e84
Martin Severin Steffensen <maritim@gmail.com>
2022-12-27 18:47:04 +0100

    Day 15 WIP. Simplification.

--- src/main/java/io/github/maritims/aoc2022/Day15.java
+++ src/main/java/io/github/maritims/aoc2022/Day15.java
@@ -66,18 +66,13 @@ public class Day15 extends Puzzle<Integer, Integer> {
             grid[sensor.getBeacon().getY() - minY][sensor.getBeacon().getX() - minX] = 'B';
         });
 
-        /*Sensor sensor = sensors.stream()
-                .filter(s -> s.getLocation().getX() == 8 && s.getLocation().getY() == 7)
-                .findFirst()
-                .orElse(null);*/
         for(Sensor sensor : sensors) {
-            for (int y = 0; y < grid.length; y++) {
-                Character[] line = grid[y];
-                for (int col = 0; col < line.length; col++) {
+            for (int row = 0; row < grid.length; row++) {
+                for (int col = 0; col < grid[row].length; col++) {
                     int x = col + minX;
-                    int d = sensor.getManhattanDistance(x, y);
-                    if (line[col] == '.' && sensor.getManhattanDistance() >= d) {
-                        line[col] = '#';
+                    int d = sensor.getManhattanDistance(x, row);
+                    if (grid[row][col] == '.' && sensor.getManhattanDistance() >= d) {
+                        grid[row][col] = '#';
                     }
                 }
             }
@@ -86,13 +81,9 @@ public class Day15 extends Puzzle<Integer, Integer> {
         render(grid);
 
         int sum = 0;
-        for(int row = 0; row < grid.length; row++) {
-            int y = row + minY;
-            if(y != 10) continue;
-            for(int col = 0; col < grid[row].length; col++) {
-                if(grid[row][col] == '#') {
-                    sum++;
-                }
+        for(int col = 0; col < grid[10 + minY].length; col++) {
+            if(grid[10 + minY][col] == '#') {
+                sum++;
             }
         }
 
