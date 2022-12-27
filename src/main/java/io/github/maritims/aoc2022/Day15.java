e731ce32bdd9ee2e7224ff8803821a1b0e0caa88
Martin Severin Steffensen <maritim@gmail.com>
2022-12-27 14:47:17 +0100

    Day 15 WIP. Drawn positions on a row where there can't be another beacon.

--- src/main/java/io/github/maritims/aoc2022/Day15.java
+++ src/main/java/io/github/maritims/aoc2022/Day15.java
@@ -3,10 +3,7 @@ package io.github.maritims.aoc2022;
 import io.github.maritims.lib.Point;
 
 import java.io.IOException;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -15,56 +12,71 @@ import static java.util.Arrays.fill;
 import static java.util.Arrays.stream;
 
 public class Day15 extends Puzzle<Integer, Integer> {
-    static class PointWithType extends Point {
-        private final Character type;
+    static class Sensor {
+        private final Point location;
+        private final Point beacon;
 
-        public PointWithType(int x, int y, Character type) {
-            super(x, y);
-            this.type = type;
+        Sensor(Point location, Point beacon) {
+            this.location = location;
+            this.beacon = beacon;
         }
 
-        public Character getType() {
-            return type;
+        public Point getLocation() {
+            return location;
         }
 
-        public int getManhattanDistance(Point destination) {
-            return Math.abs(destination.getX() - getX()) + Math.abs(destination.getY() - getY());
+        public Point getBeacon() {
+            return beacon;
+        }
+
+        public int getManhattanDistance(int x, int y) {
+            return Math.abs(location.getX() - x) + Math.abs(location.getY() - y);
+        }
+
+        public int getManhattanDistance() {
+            return getManhattanDistance(beacon.getX(), beacon.getY());
         }
     }
 
-    private static List<PointWithType> getPoints(String line) {
-        List<PointWithType> sensors = new LinkedList<>();
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    private static Sensor getSensor(String line) {
         Matcher matcher = Pattern.compile("x=([0-9\\-]+),\\sy=([0-9\\-]+)").matcher(line);
-
         matcher.find();
-        sensors.add(new PointWithType(Integer.parseInt(matcher.group(1)), Integer.parseInt(matcher.group(2)), 'S'));
-
+        Point location = new Point(Integer.parseInt(matcher.group(1)), Integer.parseInt(matcher.group(2)));
         matcher.find();
-        sensors.add(new PointWithType(Integer.parseInt(matcher.group(1)), Integer.parseInt(matcher.group(2)), 'B'));
-
-        return sensors;
+        Point beacon = new Point(Integer.parseInt(matcher.group(1)), Integer.parseInt(matcher.group(2)));
+        return new Sensor(location, beacon);
     }
 
     @Override
     public Integer solvePartOne(String filePath) throws IOException {
-        List<String> lines = getFileContent(filePath);
-        List<PointWithType> points = lines.stream()
-                .map(Day15::getPoints)
-                .flatMap(Collection::stream)
+        List<Sensor> sensors = getFileContent(filePath).stream()
+                .map(Day15::getSensor)
                 .collect(Collectors.toList());
 
-        int minX = points.stream().min(Comparator.comparing(Point::getX)).map(Point::getX).orElse(0);
-        int maxX = points.stream().max(Comparator.comparing(Point::getX)).map(Point::getX).orElse(0);
-        int minY = points.stream().min(Comparator.comparing(Point::getY)).map(Point::getY).orElse(0);
-        int maxY = points.stream().max(Comparator.comparing(Point::getY)).map(Point::getY).orElse(0);
+        int minX = sensors.stream().map(sensor -> Math.min(sensor.getLocation().getX(), sensor.getBeacon().getX())).min(Integer::compareTo).orElse(0);
+        int maxX = sensors.stream().map(sensor -> Math.max(sensor.getLocation().getX(), sensor.getBeacon().getX())).max(Integer::compareTo).orElse(0);
+        int minY = sensors.stream().map(sensor -> Math.min(sensor.getLocation().getY(), sensor.getBeacon().getY())).min(Integer::compareTo).orElse(0);
+        int maxY = sensors.stream().map(sensor -> Math.max(sensor.getLocation().getY(), sensor.getBeacon().getY())).max(Integer::compareTo).orElse(0);
 
         Character[][] grid = new Character[(maxY - minY) + 1][(maxX - minX) + 1];
         stream(grid).forEach(row -> fill(row, '.'));
-        points.forEach(point -> grid[point.getY() - minY][point.getX() - minX] = point.getType());
+        sensors.forEach(sensor -> {
+            grid[sensor.getLocation().getY() - minY][sensor.getLocation().getX() - minX] = 'S';
+            grid[sensor.getBeacon().getY() - minY][sensor.getBeacon().getX() - minX] = 'B';
+        });
 
-        for(int row = 0; row < grid.length; row++) {
-            for(int col = 0; col < grid[row].length; col++) {
-                // Can this tile be a beacon?
+        Sensor sensor = sensors.stream()
+                .filter(s -> s.getLocation().getX() == 8 && s.getLocation().getY() == 7)
+                .findFirst()
+                .orElse(null);
+        int y = sensor.getLocation().getY() - minY;
+        Character[] row = grid[y];
+        for(int col = 0; col < row.length; col++) {
+            int x = col + minX;
+            int d = sensor.getManhattanDistance(x, y);
+            if(row[col] == '.' && sensor.getManhattanDistance() >= d) {
+                row[col] = '#';
             }
         }
 
--- src/test/java/io/github/maritims/aoc2022/Day15Test.java
+++ src/test/java/io/github/maritims/aoc2022/Day15Test.java
@@ -1,8 +1,14 @@
 package io.github.maritims.aoc2022;
 
+import io.github.maritims.lib.Point;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
+
+import static io.github.maritims.aoc2022.Day15.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Day15Test extends PuzzleTest<Integer, Integer, Day15> {
     public Day15Test() {
@@ -19,5 +25,26 @@ public class Day15Test extends PuzzleTest<Integer, Integer, Day15> {
     @Override
     public Stream<Arguments> solvePartTwo() {
         return null;
+    }
+
+    public Stream<Arguments> getManhattanDistance() {
+        return Stream.of(
+                Arguments.arguments(8, 7, 2, 10, 9),
+                Arguments.arguments(8, 7, 8, -2, 9),
+                Arguments.arguments(8, 7, -1, 7, 9)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    public void getManhattanDistance(int x1, int y1, int x2, int y2, int expectedResult) {
+        // arrange
+        Sensor sensor = new Sensor(new Point(x1, y1), new Point(x2, y2));
+
+        // act
+        int result = sensor.getManhattanDistance();
+
+        // assert
+        assertEquals(expectedResult, result);
     }
 }
