416051205553249f53731619ca64a8d780525534
Martin Severin Steffensen <maritim@gmail.com>
2022-12-28 17:12:04 +0100

    Day 15 WIP. Still not finding all tiles to test.

--- src/main/java/io/github/maritims/aoc2022/Day15.java
+++ src/main/java/io/github/maritims/aoc2022/Day15.java
@@ -32,26 +32,37 @@ public class Day15 extends Puzzle<Integer, Integer> {
                 .collect(Collectors.toList());
 
         // On a specific line how many tiles cannot possibly contain a beacon?
-        LinkedHashSet<String> impossibleTiles = new LinkedHashSet<>();
+        LinkedHashSet<Integer> impossibleXes = new LinkedHashSet<>();
         for(Tuple2<Point, Point> tuple : tuples) {
             // Is this sensor on the line we're testing?
             if(tuple.getItem1().getY() == 10) {
-                impossibleTiles.add(tuple.getItem1().toString());
-            }
-
-            // Is this beacon on the line we're testing?
-            if(tuple.getItem2().getY() == 10) {
-                impossibleTiles.add(tuple.getItem2().toString());
+                impossibleXes.add(tuple.getItem2().getX());
             }
 
             // A tile is too close if distanceToTile is less than or equal to distanceToBeacon
-            int distanceToBeacon = getManhattanDistance(tuple.getItem1(), tuple.getItem2());
+            int distance = Math.abs(tuple.getItem2().getX() - tuple.getItem1().getX()) + Math.abs(tuple.getItem2().getY() - tuple.getItem1().getY());
+            int tileDistanceY = Math.abs(10 - tuple.getItem1().getY());
 
-            // How do we find all the tiles to test?
+            // The tile is possible if it's further away than the sensor-beacon distance along any axis.
+            if(tileDistanceY > distance) {
+                continue;
+            }
+
+            // Check all tiles to the left and right but no further to either side than tileDistanceX.
+            int tileDistanceX = distance - tileDistanceY;
+
+            // The current sensor is of course on an impossible tile.
+            impossibleXes.add(tuple.getItem1().getX());
+
+            // Any tile to the left is impossible.
+            IntStream.of(tuple.getItem1().getX() - tileDistanceX, tuple.getItem1().getX()).forEach(impossibleXes::add);
+
+            // Any tile to the right is impossible.
+            IntStream.of(tuple.getItem1().getX(), tuple.getItem1().getX() + tileDistanceX).forEach(impossibleXes::add);
         }
 
-        System.out.println(impossibleTiles);
-        return impossibleTiles.size();
+        System.out.println(impossibleXes);
+        return impossibleXes.size();
     }
 
     @Override
