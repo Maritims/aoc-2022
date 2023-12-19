1086211d5bce5404b9d94a8eddf2434e2d6b68f9
Martin Severin Steffensen <maritim@gmail.com>
2023-12-19 16:06:20 +0100

    2023-12-09, part 2.

--- 2023/src/main/java/io/github/maritims/aoc2023/day9/Day9.java
+++ 2023/src/main/java/io/github/maritims/aoc2023/day9/Day9.java
@@ -11,53 +11,51 @@ public class Day9 extends Day {
         super(9, useSampleData);
     }
 
-    List<List<Integer>> getFullHistory(List<List<Integer>> numbers) {
-        var iterator    = numbers.get(numbers.size() - 1).iterator();
+    protected ArrayList<List<Integer>> getSequencedStories(ArrayList<List<Integer>> sequences) {
+        var iterator    = sequences.get(sequences.size() - 1).iterator();
         var current     = iterator.next();
-        var differences = new ArrayList<Integer>();
+        var newSequence = new ArrayList<Integer>();
         var isAllZero   = true;
 
         while (iterator.hasNext()) {
             var next       = iterator.next();
             var difference = next - current;
-            differences.add(difference);
+            newSequence.add(difference);
             current = next;
 
             if (difference != 0) isAllZero = false;
         }
 
-        numbers.add(differences);
-        return isAllZero ? numbers : getFullHistory(numbers);
+        sequences.add(newSequence);
+        return isAllZero ? sequences : getSequencedStories(sequences);
     }
 
     @Override
     public long solvePartOne() {
-        var lines   = readAllLines();
-        var sum = 0;
+        var lines = readAllLines();
+        var sum   = 0;
         for (var line : lines) {
-            var numbers = Arrays.stream(line.split(" ")).map(Integer::parseInt).toList();
-            var listOfNumbers = new ArrayList<List<Integer>>();
-            listOfNumbers.add(new ArrayList<>(numbers));
+            var stories          = Arrays.stream(line.split(" ")).map(Integer::parseInt).toList();
+            var sequencedStories = new ArrayList<List<Integer>>();
+            sequencedStories.add(new ArrayList<>(stories));
+            sequencedStories = getSequencedStories(sequencedStories);
 
-            var fullHistory = getFullHistory(listOfNumbers);
-
-            for (var i = fullHistory.size(); i > 0; i--) {
+            for (var i = sequencedStories.size(); i > 0; i--) {
                 // Take care of the all-zero row first.
-                if (i == fullHistory.size()) {
-                    fullHistory.get(i - 1).add(0);
+                if (i == sequencedStories.size()) {
+                    sequencedStories.get(i - 1).add(0);
                     continue;
                 }
 
-                var historyBelowCurrent = fullHistory.get(i);
-                var increment           = historyBelowCurrent.get(historyBelowCurrent.size() - 1);
-
-                var currentHistory = fullHistory.get(i - 1);
-                var extrapolation  = currentHistory.get(currentHistory.size() - 1) + increment;
-                currentHistory.add(extrapolation);
+                var sequenceBelowCurrent = sequencedStories.get(i);
+                var increment            = sequenceBelowCurrent.get(sequenceBelowCurrent.size() - 1);
+                var currentSequence      = sequencedStories.get(i - 1);
+                var extrapolation        = currentSequence.get(currentSequence.size() - 1) + increment;
+                currentSequence.add(extrapolation);
             }
 
-            var originalHistory = fullHistory.get(0);
-            var extrapolation = originalHistory.get(originalHistory.size() - 1);
+            var sequence      = sequencedStories.get(0);
+            var extrapolation = sequence.get(sequence.size() - 1);
             sum += extrapolation;
         }
 
@@ -66,6 +64,33 @@ public class Day9 extends Day {
 
     @Override
     public long solvePartTwo() {
-        return 0;
+        var lines = readAllLines();
+        var sum   = 0;
+        for (var line : lines) {
+            var stories          = Arrays.stream(line.split(" ")).map(Integer::parseInt).toList();
+            var sequencedStories = new ArrayList<List<Integer>>();
+            sequencedStories.add(new ArrayList<>(stories));
+            sequencedStories = getSequencedStories(sequencedStories);
+
+            for (var i = sequencedStories.size(); i > 0; i--) {
+                // Take care of the all-zero row first.
+                if (i == sequencedStories.size()) {
+                    sequencedStories.get(i - 1).add(0, 0);
+                    continue;
+                }
+
+                var sequenceBelowCurrent = sequencedStories.get(i);
+                var increment            = sequenceBelowCurrent.get(0);
+                var currentSequence      = sequencedStories.get(i - 1);
+                var extrapolation        = currentSequence.get(0) - increment;
+                currentSequence.add(0, extrapolation);
+            }
+
+            var sequence      = sequencedStories.get(0);
+            var extrapolation = sequence.get(0);
+            sum += extrapolation;
+        }
+
+        return sum;
     }
 }
--- 2023/src/test/java/io/github/maritims/aoc2023/day9/Day9Test.java
+++ 2023/src/test/java/io/github/maritims/aoc2023/day9/Day9Test.java
@@ -18,9 +18,22 @@ class Day9Test {
         );
     }
 
+    public static Stream<Arguments> solvePartTwo() {
+        return Stream.of(
+            arguments(true, 2),
+            arguments(false, 0)
+        );
+    }
+
     @ParameterizedTest
     @MethodSource
     void solvePartOne(boolean useSampleData, int expectedResult) {
         assertEquals(expectedResult, new Day9(useSampleData).solvePartOne());
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void solvePartTwo(boolean useSampleData, int expectedResult) {
+        assertEquals(expectedResult, new Day9(useSampleData).solvePartTwo());
     }
 }
