439f460ac2d0ca8fb3e4731b06596c5f2be91349
Martin Severin Steffensen <maritim@gmail.com>
2023-12-21 01:41:21 +0100

    2023-12-12 WIP.

---
+++ 2023/src/main/java/io/github/maritims/aoc2023/day12/Day12.java
@@ -0,0 +1,19 @@
+package io.github.maritims.aoc2023.day12;
+
+import io.github.maritims.toolbox.Day;
+
+public class Day12 extends Day {
+    protected Day12(boolean useSampleData) {
+        super(12, useSampleData);
+    }
+
+    @Override
+    public long solvePartOne() {
+        return 0;
+    }
+
+    @Override
+    public long solvePartTwo() {
+        return 0;
+    }
+}
---
+++ 2023/src/test/java/io/github/maritims/aoc2023/day12/Day12Test.java
@@ -0,0 +1,16 @@
+package io.github.maritims.aoc2023.day12;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day12Test {
+
+    @Test
+    void solvePartOne() {
+    }
+
+    @Test
+    void solvePartTwo() {
+    }
+}
