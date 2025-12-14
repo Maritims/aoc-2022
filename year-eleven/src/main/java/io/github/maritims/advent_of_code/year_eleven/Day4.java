915e65a3c05ee591b4310739f0fb75cd9abcfb9a
Maritim <maritim@gmail.com>
2025-12-14 02:28:25 +0100

    2025.04.2

--- year-eleven/src/main/java/io/github/maritims/advent_of_code/year_eleven/Day4.java
+++ year-eleven/src/main/java/io/github/maritims/advent_of_code/year_eleven/Day4.java
@@ -2,14 +2,15 @@ package io.github.maritims.advent_of_code.year_eleven;
 
 import io.github.maritims.advent_of_code.common.PuzzleSolver;
 import io.github.maritims.advent_of_code.common.geometry.Grid2D;
+import io.github.maritims.advent_of_code.common.geometry.Point2D;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Day4 extends PuzzleSolver<Integer, Integer> {
-    @Override
-    public Integer solveFirstPart() {
-        var input = loadInput();
-        var grid  = new Grid2D<>(input.get(0).length(), input.size(), () -> '.');
+    Grid2D<Character> createGrid(List<String> input) {
+        var grid = new Grid2D<>(input.get(0).length(), input.size(), () -> '.');
 
         for (var row = 0; row < input.size(); row++) {
             for (var col = 0; col < input.get(row).length(); col++) {
@@ -20,7 +21,13 @@ public class Day4 extends PuzzleSolver<Integer, Integer> {
             }
         }
 
+        return grid;
+    }
+
+    int walkGrid(Grid2D<Character> grid, boolean clearAccessibleCells) {
         var accessiblePaperRolls = new AtomicInteger(0);
+        var cellsToClear         = new HashSet<Point2D>();
+
         grid.walkGrid((row, col, value, surroundingPoints) -> {
             if (value == '@') {
                 var occupiedSurroundingPoints = surroundingPoints.stream()
@@ -28,15 +35,38 @@ public class Day4 extends PuzzleSolver<Integer, Integer> {
                         .count();
                 if (occupiedSurroundingPoints < 4) {
                     accessiblePaperRolls.incrementAndGet();
+                    if (clearAccessibleCells) {
+                        cellsToClear.add(new Point2D(col, row));
+                    }
                 }
             }
         });
+
+        if (clearAccessibleCells) {
+            cellsToClear.forEach(cellToClear -> grid.set(cellToClear.row(), cellToClear.col(), '.'));
+        }
 
         return accessiblePaperRolls.get();
     }
 
     @Override
+    public Integer solveFirstPart() {
+        var input = loadInput();
+        var grid  = createGrid(input);
+        return walkGrid(grid, false);
+    }
+
+    @Override
     public Integer solveSecondPart() {
-        return 0;
+        var input  = loadInput();
+        var grid   = createGrid(input);
+        var walked = 0;
+        var sum    = 0;
+
+        while (((walked = walkGrid(grid, true)) > 0)) {
+            sum += walked;
+        }
+
+        return sum;
     }
 }
--- year-eleven/src/test/java/io/github/maritims/advent_of_code/year_eleven/Day4Test.java
+++ year-eleven/src/test/java/io/github/maritims/advent_of_code/year_eleven/Day4Test.java
@@ -2,8 +2,12 @@ package io.github.maritims.advent_of_code.year_eleven;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.doReturn;
@@ -11,6 +15,124 @@ import static org.mockito.Mockito.spy;
 
 class Day4Test {
     Day4 sut;
+
+    public static Stream<Arguments> walkGrid() {
+        return Stream.of(
+                Arguments.of("""
+                                     ..@@.@@@@.
+                                     @@@.@.@.@@
+                                     @@@@@.@.@@
+                                     @.@@@@..@.
+                                     @@.@@@@.@@
+                                     .@@@@@@@.@
+                                     .@.@.@.@@@
+                                     @.@@@.@@@@
+                                     .@@@@@@@@.
+                                     @.@.@@@.@.
+                                     
+                                     """, 13),
+                Arguments.of("""
+                                     ..xx.xx@x.
+                                     x@@.@.@.@@
+                                     @@@@@.x.@@
+                                     @.@@@@..@.
+                                     x@.@@@@.@x
+                                     .@@@@@@@.@
+                                     .@.@.@.@@@
+                                     x.@@@.@@@@
+                                     .@@@@@@@@.
+                                     x.x.@@@.x.""", 12),
+                Arguments.of("""
+                                     .......x..
+                                     .@@.x.x.@x
+                                     x@@@@...@@
+                                     x.@@@@..x.
+                                     .@.@@@@.x.
+                                     .x@@@@@@.x
+                                     .x.@.@.@@@
+                                     ..@@@.@@@@
+                                     .x@@@@@@@.
+                                     ....@@@...""", 7),
+                Arguments.of("""
+                                     ..........
+                                     .x@.....x.
+                                     .@@@@...xx
+                                     ..@@@@....
+                                     .x.@@@@...
+                                     ..@@@@@@..
+                                     ...@.@.@@x
+                                     ..@@@.@@@@
+                                     ..x@@@@@@.
+                                     ....@@@...
+                                     """, 5),
+                Arguments.of("""
+                                     ..........
+                                     ..x.......
+                                     .x@@@.....
+                                     ..@@@@....
+                                     ...@@@@...
+                                     ..x@@@@@..
+                                     ...@.@.@@.
+                                     ..x@@.@@@x
+                                     ...@@@@@@.
+                                     ....@@@...""", 2),
+                Arguments.of("""
+                                     ..........
+                                     ..........
+                                     ..x@@.....
+                                     ..@@@@....
+                                     ...@@@@...
+                                     ...@@@@@..
+                                     ...@.@.@@.
+                                     ...@@.@@@.
+                                     ...@@@@@x.
+                                     ....@@@...""", 1),
+                Arguments.of("""
+                                     ..........
+                                     ..........
+                                     ...@@.....
+                                     ..x@@@....
+                                     ...@@@@...
+                                     ...@@@@@..
+                                     ...@.@.@@.
+                                     ...@@.@@@.
+                                     ...@@@@@..
+                                     ....@@@...""", 1),
+                Arguments.of("""
+                                     ..........
+                                     ..........
+                                     ...x@.....
+                                     ...@@@....
+                                     ...@@@@...
+                                     ...@@@@@..
+                                     ...@.@.@@.
+                                     ...@@.@@@.
+                                     ...@@@@@..
+                                     ....@@@...""", 1),
+                Arguments.of("""
+                                     ..........
+                                     ..........
+                                     ....x.....
+                                     ...@@@....
+                                     ...@@@@...
+                                     ...@@@@@..
+                                     ...@.@.@@.
+                                     ...@@.@@@.
+                                     ...@@@@@..
+                                     ....@@@...""", 1),
+                Arguments.of("""
+                                     ..........
+                                     ..........
+                                     ..........
+                                     ...x@@....
+                                     ...@@@@...
+                                     ...@@@@@..
+                                     ...@.@.@@.
+                                     ...@@.@@@.
+                                     ...@@@@@..
+                                     ....@@@...""", 0)
+        );
+    }
 
     @BeforeEach
     void setUp() {
@@ -38,10 +160,19 @@ class Day4Test {
 
     @Test
     void solveFirstPart() {
-        assertEquals(100, sut.solveFirstPart());
+        assertEquals(1493, sut.solveFirstPart());
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void walkGrid(String input, int expectedResult) {
+        var grid   = sut.createGrid(Arrays.stream(input.split("\n")).toList());
+        var result = sut.walkGrid(grid, false);
+        assertEquals(expectedResult, result);
     }
 
     @Test
     void solveSecondPart() {
+        assertEquals(9194, sut.solveSecondPart());
     }
 }
