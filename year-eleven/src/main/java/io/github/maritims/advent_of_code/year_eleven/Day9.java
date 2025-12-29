423b7ac4b2d1fa0fe081c5c6cb6080c4539c81f4
maritims <maritim@gmail.com>
2025-12-29 22:48:34 +0100

    2025.9

--- year-eleven/src/main/java/io/github/maritims/advent_of_code/year_eleven/Day9.java
+++ year-eleven/src/main/java/io/github/maritims/advent_of_code/year_eleven/Day9.java
@@ -1,65 +1,54 @@
 package io.github.maritims.advent_of_code.year_eleven;
 
 import io.github.maritims.advent_of_code.common.PuzzleSolver;
-import io.github.maritims.advent_of_code.common.geometry.Line2D;
 import io.github.maritims.advent_of_code.common.geometry.Point2D;
 import io.github.maritims.advent_of_code.common.geometry.Polygon;
 import io.github.maritims.advent_of_code.common.geometry.Rectangle;
+import io.github.maritims.advent_of_code.common.tuples.Tuple2;
 
 public class Day9 extends PuzzleSolver<Long, Long> {
-    @Override
-    public Long solveFirstPart() {
-        var points = loadInput()
-                .stream()
-                .map(Point2D::fromString)
-                .toList();
-        var maxArea = 0.0;
+    private Tuple2<Long, Long> solution;
 
-        for(var i = 0; i < points.size(); i++) {
-            for(var j = i + 1; j < points.size(); j++) {
-                var p1 = points.get(i);
-                var p2 = points.get(j);
-                var line = Line2D.of(p1, p2);
 
-                // Do we have a diagonal?
-                if(line.isDiagonal()) {
-                    var rectangle = new Rectangle(p1, p2);
-                    maxArea = Math.max(maxArea, rectangle.getGridArea());
+    private Tuple2<Long, Long> solveBothParts() {
+        if (solution == null) {
+            var points                  = Point2D.fromStrings(loadInput());
+            var polygonWithColoredTiles = new Polygon(points);
+            var maxAreaForFirstPart     = 0.0;
+            var maxAreaForSecondPart    = 0.0;
+
+            for (var i = 0; i < points.size(); i++) {
+                for (var j = i + 1; j < points.size(); j++) {
+                    var p1   = points.get(i);
+                    var p2   = points.get(j);
+                    var line = p1.lineTo(p2);
+
+                    // Do we have a diagonal?
+                    if (line.isDiagonal()) {
+                        var rectangle = new Rectangle(p1, p2);
+
+                        maxAreaForFirstPart = Math.max(maxAreaForFirstPart, rectangle.getGridArea());
+
+                        // Don't bother with math if the area is smaller anyway.
+                        if (rectangle.getGridArea() > maxAreaForSecondPart && polygonWithColoredTiles.containsRectangle(rectangle)) {
+                            maxAreaForSecondPart = rectangle.getGridArea();
+                        }
+                    }
                 }
             }
-        }
 
-        return (long) maxArea;
+            solution = new Tuple2<>((long) maxAreaForFirstPart, (long) maxAreaForSecondPart);
+        }
+        return solution;
+    }
+
+    @Override
+    public Long solveFirstPart() {
+        return solveBothParts().item1();
     }
 
     @Override
     public Long solveSecondPart() {
-        var points = Point2D.fromStrings(loadInput());
-        var polygonWithColoredTiles = new Polygon(points);
-        var maxArea = 0.0;
-
-        for(var i = 0; i < points.size(); i++) {
-            for(var j = i + 1; j < points.size(); j++) {
-                var p1 = points.get(i);
-                var p2 = points.get(j);
-                var line = p1.lineTo(p2);
-
-                // Do we have a diagonal?
-                if(line.isDiagonal()) {
-                    var rectangle = new Rectangle(p1, p2);
-
-                    // Don't bother with math if the area is smaller anyway.
-                    if (maxArea > rectangle.getGridArea()) {
-                        continue;
-                    }
-
-                    if(polygonWithColoredTiles.containsRectangle(rectangle)) {
-                        maxArea = rectangle.getGridArea();
-                    }
-                }
-            }
-        }
-
-        return (long) maxArea;
+        return solveBothParts().item2();
     }
 }
