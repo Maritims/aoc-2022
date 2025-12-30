baa80143fcd9152fb1114e021c17b9f71da79383
maritims <maritim@gmail.com>
2025-12-30 23:55:24 +0100

    2025.11.1

---
+++ year-eleven/src/main/java/io/github/maritims/advent_of_code/year_eleven/Day11.java
@@ -0,0 +1,52 @@
+package io.github.maritims.advent_of_code.year_eleven;
+
+import io.github.maritims.advent_of_code.common.PuzzleSolver;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Day11 extends PuzzleSolver<Long, Long> {
+    /**
+     * Count all the paths in the directed acyclic graph (DAG).
+     *
+     * @param graph       A directed acyclic graph.
+     * @param memo        Memoization map.
+     * @param current     Where we're at.
+     * @param destination WHere we're going.
+     * @return The number of all paths leading from current to destination.
+     */
+    private static long countAllPaths(Map<String, List<String>> graph, Map<String, Long> memo, String current, String destination) {
+        if (current.equals(destination)) {
+            return 1;
+        }
+
+        if (memo.containsKey(current)) {
+            return memo.get(current);
+        }
+
+        var neighbours = graph.getOrDefault(current, new ArrayList<>());
+        var count      = neighbours.stream().mapToLong(neighbour -> countAllPaths(graph, memo, neighbour, destination)).sum();
+        memo.put(current, count);
+        return count;
+    }
+
+    @Override
+    public Long solveFirstPart() {
+        var graph = loadInput()
+                .stream()
+                .map(line -> line.split(" "))
+                .map(parts -> {
+                    var input   = parts[0].substring(0, parts[0].length() - 1);
+                    var outputs = Arrays.stream(parts).skip(1).toList();
+                    return Map.entry(input, outputs);
+                })
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        var memo = new HashMap<String, Long>();
+        return countAllPaths(graph, memo, "you", "out");
+    }
+
+    @Override
+    public Long solveSecondPart() {
+        return 0L;
+    }
+}
---
+++ year-eleven/src/test/java/io/github/maritims/advent_of_code/year_eleven/Day11Test.java
@@ -0,0 +1,34 @@
+package io.github.maritims.advent_of_code.year_eleven;
+
+import io.github.maritims.advent_of_code.common.testing.DayTest;
+import org.junit.jupiter.api.Test;
+import org.mockito.Spy;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Day11Test extends DayTest<Day11> {
+    @Spy
+    Day11 sut;
+
+    @Test
+    public void solveFirstPart_withSampleInput() {
+        mockLoadInput(sut, """
+                aaa: you hhh
+                you: bbb ccc
+                bbb: ddd eee
+                ccc: ddd eee fff
+                ddd: ggg
+                eee: out
+                fff: out
+                ggg: out
+                hhh: ccc fff iii
+                iii: out
+                """);
+        assertEquals(5, sut.solveFirstPart());
+    }
+
+    @Test
+    public void solveFirstPart() {
+        assertEquals(662, sut.solveFirstPart());
+    }
+}
